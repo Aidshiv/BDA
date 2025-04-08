6dee5d12a0333bb25ff88c96681d34ee193a5bcf
Aidshiv <163630330+Aidshiv@users.noreply.github.com>
2025-04-08 23:29:46 +0530

    Update prog5.py

--- prog5.py
+++ prog5.py
@@ -1,20 +1,24 @@
-$ sudo dnf install -y mongodb mongodb-server
 
-
-$ sudo systemctl start mongod
-
-
-$ sudo systemctl enable mongod
-
-
-$ sudo systemctl status mongod
-
-
-$ sudo dnf install mongodb-mongosh
-
-
-$ mongosh
-
+$sudo dnf install -y mongodb mongodb-server
+$sudo systemctl start mongod
+$sudo systemctl enable mongod
+$sudo systemctl status mongod
+$sudo dnf install mongodb-mongosh
+Start the MongoDB shell by running:
+$mangosh
+Select a database
+$use testDB
+Sample Data:
+[
+ { "_id": 1, "name": "Alice", "age": 22, "marks": 85 },
+ { "_id": 2, "name": "Bob", "age": 21, "marks": 78 },
+ { "_id": 3, "name": "Charlie", "age": 23, "marks": 92 },
+ { "_id": 4, "name": "David", "age": 20, "marks": 88 },
+ { "_id": 5, "name": "Eve", "age": 22, "marks": 76 }
+]
+Create students collection using above sample data
+db.createCollection('Students');
+db.student.insertOne({ "_id": 1, "name": "Alice", "age": 22, "marks": 85 })
 
 $ use testDB
 
